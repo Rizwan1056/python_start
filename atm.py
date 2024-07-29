8ed1b13821e3017e5414c16eed9f5c566e81d62c
Rizwan1056 <rizwi1056@gmail.com>
2024-07-30 00:34:13 +0500

    updated.

--- atm.py
+++ atm.py
@@ -7,21 +7,21 @@ while True:
     except ValueError:
         print("Invalid input. Please enter a valid integer.")
 
-fiveThousand =rupee // 5000 
-thousand = (rupee % 5000) // 1000
-fiveHundred = (rupee % 1000) //500
-hundred = (rupee % 500) // 100
-fifty = (rupee % 100) // 50
-ten = (rupee % 50) // 10
-ones = (rupee % 10) // 1
+    fiveThousand =rupee // 5000 
+    thousand = (rupee % 5000) // 1000
+    fiveHundred = (rupee % 1000) //500
+    hundred = (rupee % 500) // 100
+    fifty = (rupee % 100) // 50
+    ten = (rupee % 50) // 10
+    ones = (rupee % 10) // 1
 
 
-print("\n\t5000: ",fiveThousand)
-print("\t1000 :",thousand)
-print("\t500  :",fiveHundred)
-print("\t100  :",hundred)
-print("\t50   :",fifty)
-print("\t10   :",ten)
-print("\t1    :",ones)
+    print("\n\t5000: ",fiveThousand)
+    print("\t1000 :",thousand)
+    print("\t500  :",fiveHundred)
+    print("\t100  :",hundred)
+    print("\t50   :",fifty)
+    print("\t10   :",ten)
+    print("\t1    :",ones)
 
 
